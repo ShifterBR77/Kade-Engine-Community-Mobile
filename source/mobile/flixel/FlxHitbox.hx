640496af87c88fdaf291ff7b62bd76f32aaece95
Lily <egzozu.be.bas@gmail.com>
2024-05-27 11:19:52 +0300

    Update FlxHitbox.hx

--- source/mobile/flixel/FlxHitbox.hx
+++ source/mobile/flixel/FlxHitbox.hx
@@ -63,15 +63,16 @@ class FlxHitbox extends FlxSpriteGroup
 		hint.immovable = true;
 		hint.scrollFactor.set();
 		hint.alpha = 0.00001;
-		hint.onDown.callback = hint.onOver.callback = function()
-		{
-			if (hint.alpha != 0.2)
-				hint.alpha = 0.2;
-		}
-		hint.onUp.callback = hint.onOut.callback = function()
-		{
-			if (hint.alpha != 0.00001)
-				hint.alpha = 0.00001;
+		if (FlxG.save.data.hitboxType != 2) {
+			hint.onDown.callback = hint.onOver.callback = () ->
+				{
+					if (hint.alpha != 0.2) hint.alpha = 0.2;
+				}
+				
+			hint.onUp.callback = hint.onOut.callback = () ->
+				{
+					if (hint.alpha != 0.00001) hint.alpha = 0.00001;
+				}
 		}
 		#if FLX_DEBUG
 		hint.ignoreDrawDebug = true;
@@ -81,12 +82,25 @@ class FlxHitbox extends FlxSpriteGroup
 
 	private function createHintGraphic(Width:Int, Height:Int, Color:Int = 0xFFFFFF):BitmapData
 	{
+		var guh:Float = FlxG.save.data.mobileCAlpha;
+		if (guh >= 0.9)
+			guh = FlxG.save.data.mobileCAlpha - 0.07;
 		var shape:Shape = new Shape();
 		shape.graphics.beginFill(Color);
-		shape.graphics.lineStyle(10, Color, 1);
-		shape.graphics.drawRect(0, 0, Width, Height);
-		shape.graphics.endFill();
-
+		if (FlxG.save.data.hitboxType == '0') {
+			shape.graphics.lineStyle(3, Color, 1);
+			shape.graphics.drawRect(0, 0, Width, Height);
+			shape.graphics.lineStyle(0, 0, 0);
+			shape.graphics.drawRect(3, 3, Width - 6, Height - 6);
+			shape.graphics.endFill();
+			shape.graphics.beginGradientFill(RADIAL, [Color, FlxColor.TRANSPARENT], [guh, 0], [0, 255], null, null, null, 0.5);
+			shape.graphics.drawRect(3, 3, Width - 6, Height - 6);
+			shape.graphics.endFill();
+		} else {
+			shape.graphics.lineStyle(10, Color, 1);
+			shape.graphics.drawRect(0, 0, Width, Height);
+			shape.graphics.endFill();
+		}
 		var bitmap:BitmapData = new BitmapData(Width, Height, true, 0);
 		bitmap.draw(shape, true);
 		return bitmap;
