97713a207feab7d7328a9a962a578e6ff5ad8360
Lily <egzozu.be.bas@gmail.com>
2024-05-27 11:05:40 +0300

    Update Options.hx

--- source/mobile/Options.hx
+++ source/mobile/Options.hx
@@ -8,12 +8,13 @@ class MobileControlsOption extends Option
 	{
 		super();
 		acceptType = true;
-		description = desc;
 		if (OptionsMenu.isInPause)
 		{
 			blocked = true;
 			description = pauseDesc;
 		}
+		else
+			description = desc;
 	}
 
 	public override function press():Bool
@@ -27,5 +28,117 @@ class MobileControlsOption extends Option
 	private override function updateDisplay():String
 	{
 		return "Mobile Controls";
+	}
+}
+
+class ScreensaverOption extends Option
+{
+	public function new(desc:String)
+	{
+		super();
+		description = desc;
+	}
+
+	public override function left():Bool
+	{
+		lime.system.System.allowScreenTimeout = !lime.system.System.allowScreenTimeout;
+		display = updateDisplay();
+		return true;
+	}
+
+	public override function right():Bool
+	{
+		left();
+		return true;
+	}
+
+	private override function updateDisplay():String
+	{
+		return "Allow Phone Screensaver: < " + (!lime.system.System.allowScreenTimeout ? "Disabled" : "Enabled") + " >";
+	}
+}
+
+class MobileControlsOpacityOption extends Option
+{
+	public function new(desc:String)
+	{
+		super();
+		description = desc;
+		acceptValues = true;
+	}
+
+	private function check():Void
+	{
+		if (FlxG.save.data.mobileCAlpha < 0)
+			FlxG.save.data.mobileCAlpha = 0;
+
+		if (FlxG.save.data.mobileCAlpha > 1)
+			FlxG.save.data.mobileCAlpha = 1;
+	}
+
+	override function right():Bool
+	{
+		FlxG.save.data.mobileCAlpha += 0.1;
+
+		check();
+
+		return true;
+	}
+
+	override function left():Bool
+	{
+		FlxG.save.data.mobileCAlpha -= 0.1;
+
+		check();
+
+		return true;
+	}
+
+	override function getValue():String
+	{
+		return "Mobile Controls Opacity: " + HelperFunctions.truncateFloat(FlxG.save.data.mobileCAlpha, 1);
+	}
+}
+
+class HitboxDesignOption extends Option
+{
+	final hintOptions:Array<String> = ["Gradient", "No Gradient", "Hidden"];
+	public function new(desc:String)
+	{
+		super();
+		if (OptionsMenu.isInPause)
+		{
+			blocked = true;
+			description = pauseDesc;
+		}
+		else
+			description = desc;
+	}
+
+	public override function left():Bool
+	{
+		if (OptionsMenu.isInPause)
+			return false;
+		FlxG.save.data.hitboxType--;
+		if (FlxG.save.data.hitboxType < 0)
+			FlxG.save.data.hitboxType = hintOptions.length - 1;
+		display = updateDisplay();
+		return true;
+	}
+
+	public override function right():Bool
+	{
+		if (OptionsMenu.isInPause)
+			return false;
+		FlxG.save.data.hitboxType++;
+		if (FlxG.save.data.hitboxType > hintOptions.length - 1)
+			FlxG.save.data.hitboxType = 0;
+		display = updateDisplay();
+		return true;
+	}
+
+	public override function getValue():String
+	{
+		return "Current Hitbox Design: < " + hintOptions[FlxG.save.data.noteskin] + " >";
 	}
 }
